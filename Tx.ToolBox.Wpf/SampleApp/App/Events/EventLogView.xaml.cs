d3d4ef832a958625b2ea02aea6b4955bfdd2eb6b
Telliax <qtgui@yandex.ru>
2018-01-26 22:46:29 +0300

    missing files

---
+++ Tx.ToolBox.Wpf/SampleApp/App/Events/EventLogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Tx.ToolBox.Wpf.SampleApp.App.Log
+{
+    /// <summary>
+    /// Interaction logic for EventLogView.xaml
+    /// </summary>
+    public partial class EventLogView : UserControl
+    {
+        public EventLogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tx.ToolBox.Wpf/SampleApp/App/Events/EventLogViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using Tx.ToolBox.Messaging;
+using Tx.ToolBox.Wpf.Mvvm;
+
+namespace Tx.ToolBox.Wpf.SampleApp.App.Log
+{
+    class EventLogViewModel : ViewModelBase, IListener<LogMessage>
+    {
+        public EventLogViewModel(Dispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        public ObservableCollection<MessageViewModel> Events { get; } = new ObservableCollection<MessageViewModel>();
+
+        void IListener<LogMessage>.Handle(LogMessage message)
+        {
+            var vm = new MessageViewModel(message);
+            _dispatcher.BeginInvoke(() =>
+            {
+                Events.Add(vm);
+                if (Events.Count > MaxSize)
+                {
+                    Events.RemoveAt(0);
+                }
+            });
+        }
+
+        private const int MaxSize = 30;
+        private readonly Dispatcher _dispatcher;
+
+        public class MessageViewModel : ViewModelBase
+        {
+            public MessageViewModel(LogMessage message)
+            {
+                _message = message;
+            }
+
+            public bool IsError => _message.Type == LogMessageType.Error;
+
+            public override string ToString()
+            {
+                return $"[{_message.TimeStamp:T}][{_message.Type}] {_message.Message}";
+            }
+
+            private readonly LogMessage _message;
+        }
+    }
+}
